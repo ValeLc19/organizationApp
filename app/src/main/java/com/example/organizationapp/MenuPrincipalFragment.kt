8250159b3e7a6eee8580aab341f509314c9d8e20
Valeria <valerialopezc.19@gmail.com>
2023-04-14 13:05:04 -0500

    Navigation from menu principal fragment to food fragment

--- app/src/main/java/com/example/organizationapp/MenuPrincipalFragment.kt
+++ app/src/main/java/com/example/organizationapp/MenuPrincipalFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,12 @@ class MenuPrincipalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.menu_principal_fragment, container, false)
+        val view =  inflater.inflate(R.layout.menu_principal_fragment, container, false)
+        view.findViewById<ImageButton>(R.id.food_button).setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_menuPrincipalFragment_to_foodFragment)
+        }
+        return view
+
     }
 
     companion object {
